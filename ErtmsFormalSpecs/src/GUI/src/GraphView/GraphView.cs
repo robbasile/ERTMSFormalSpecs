e69fb7b561608e652c4992f2180b78cdbc9b3de5
LaurentFerier <laurent@ertmssolutions.com>
2015-09-24 16:41:26 +0200

    Refresh graph views after each step execution

--- ErtmsFormalSpecs/src/GUI/src/GraphView/GraphView.cs
+++ ErtmsFormalSpecs/src/GUI/src/GraphView/GraphView.cs
@@ -138,6 +138,19 @@ namespace GUI.GraphView
         }
 
         /// <summary>
+        ///     Indicates that a change event should be displayed
+        /// </summary>
+        /// <param name="modelElement"></param>
+        /// <param name="changeKind"></param>
+        /// <returns></returns>
+        protected override bool ShouldDisplayChange(IModelElement modelElement, Context.ChangeKind changeKind)
+        {
+            bool retVal = modelElement == null || DisplayedModel == null || DisplayedModel.IsParent(modelElement);
+
+            return retVal;
+        }
+
+        /// <summary>
         ///     Allows to refresh the view, when the value of a model changed
         /// </summary>
         /// <param name="modelElement"></param>
