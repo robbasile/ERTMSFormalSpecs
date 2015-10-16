19f00f8884b1c7046a6fbd3bdc628962deecf3c6
LaurentFerier <laurent@ertmssolutions.com>
2015-10-16 18:03:25 +0200

    Version 1.0.67

--- ErtmsFormalSpecs/src/Importers/Properties/AssemblyInfo.cs
+++ ErtmsFormalSpecs/src/Importers/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("1.0.66.*")]
-[assembly: AssemblyFileVersion("1.0.66")]
+[assembly: AssemblyVersion("1.0.67.*")]
+[assembly: AssemblyFileVersion("1.0.67")]
--- ErtmsFormalSpecs/src/Reports/Properties/AssemblyInfo.cs
+++ ErtmsFormalSpecs/src/Reports/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.69.*")]
-[assembly: AssemblyFileVersion("1.0.69")]
+[assembly: AssemblyVersion("1.0.71.*")]
+[assembly: AssemblyFileVersion("1.0.71")]
