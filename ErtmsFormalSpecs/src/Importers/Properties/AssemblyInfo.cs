8453d4f7a95d6bc49d1db52087b5298632a3d9b4
Svitlana Lukicheva <svitlana@ertmssolutions.com>
2015-10-29 18:20:15 +0100

    Version 1.0.68

--- ErtmsFormalSpecs/src/Importers/Properties/AssemblyInfo.cs
+++ ErtmsFormalSpecs/src/Importers/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("1.0.67.*")]
-[assembly: AssemblyFileVersion("1.0.67")]
+[assembly: AssemblyVersion("1.0.68.*")]
+[assembly: AssemblyFileVersion("1.0.68")]
