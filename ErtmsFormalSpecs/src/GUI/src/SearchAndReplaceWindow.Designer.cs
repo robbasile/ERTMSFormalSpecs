f4d8be5442aa32e47cb7d899db21ba372b7249a8
GautierBerck <gberck@ertmssolutions.com>
2015-11-16 17:42:52 +0100

    Implementation: recursive string search and replace implementation. Doesn't seem to work yet.

---
+++ ErtmsFormalSpecs/src/GUI/src/SearchAndReplaceWindow.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace GUI
+{
+    partial class SearchAndReplaceWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.replaceTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Search string";
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(121, 12);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(286, 20);
+            this.searchTextBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(438, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Replace";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Replace string";
+            // 
+            // replaceTextBox
+            // 
+            this.replaceTextBox.Location = new System.Drawing.Point(121, 59);
+            this.replaceTextBox.Name = "replaceTextBox";
+            this.replaceTextBox.Size = new System.Drawing.Size(286, 20);
+            this.replaceTextBox.TabIndex = 4;
+            // 
+            // SearchAndReplaceWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 100);
+            this.Controls.Add(this.replaceTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "SearchAndReplaceWindow";
+            this.Text = "SearchAndReplaceWindow";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox replaceTextBox;
+    }
+}
---
+++ ErtmsFormalSpecs/src/GUI/src/SearchAndReplaceWindow.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class SearchAndReplaceWindow : Form
+    {
+        /// <summary>
+        /// Result of this dialog
+        /// </summary>
+        DialogResult Result { get; set; }
+
+        public SearchAndReplaceWindow()
+        {
+            InitializeComponent();
+            Result = DialogResult.Abort;
+        }
+
+        public string SearchString
+        {
+            get {
+                return searchTextBox.Text;
+            }
+        }
+
+        public string ReplaceString
+        {
+            get
+            {
+                return replaceTextBox.Text;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Result = DialogResult.OK;
+            Close();
+        }
+    }
+}
