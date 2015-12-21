3565b10d5997fa2d34167e66ddef3f6eafd28005
Svitlana Lukicheva <svitlana@ertmssolutions.com>
2015-12-21 16:35:16 +0100

    The default value of the property "Check for compatible changes" is set to true

--- ErtmsFormalSpecs/src/GUI/Properties/Settings.Designer.cs
+++ ErtmsFormalSpecs/src/GUI/Properties/Settings.Designer.cs
@@ -121,7 +121,7 @@ namespace GUI.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool CheckForCompatibleChanges {
             get {
                 return ((bool)(this["CheckForCompatibleChanges"]));
