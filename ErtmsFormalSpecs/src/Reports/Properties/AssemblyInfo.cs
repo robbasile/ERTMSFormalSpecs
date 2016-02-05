0c4199809ab1cf91866b3c1de300df0fd1146c64
JamesOakey <james@ertmssolutions.com>
2016-02-05 12:22:09 +0100

    Version 1.0.71

--- ErtmsFormalSpecs/src/Reports/Properties/AssemblyInfo.cs
+++ ErtmsFormalSpecs/src/Reports/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.79.*")]
-[assembly: AssemblyFileVersion("1.0.79")]
+[assembly: AssemblyVersion("1.0.81.*")]
+[assembly: AssemblyFileVersion("1.0.81")]
