4b5aa91ae0bf5ffa5c3b33d0e03d597b7cead7cf
GautierBerck <gberck@ertmssolutions.com>
2016-04-21 10:44:51 +0200

    Rollback automatic changes

--- ErtmsFormalSpecs/src/EFSTester/Properties/AssemblyInfo.cs
+++ ErtmsFormalSpecs/src/EFSTester/Properties/AssemblyInfo.cs
@@ -54,5 +54,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.75.*")]
-[assembly: AssemblyFileVersion("1.0.75")]
+[assembly: AssemblyVersion("1.0.74.*")]
+[assembly: AssemblyFileVersion("1.0.74")]
--- ErtmsFormalSpecs/src/Importers/Properties/AssemblyInfo.cs
+++ ErtmsFormalSpecs/src/Importers/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("1.0.75.*")]
-[assembly: AssemblyFileVersion("1.0.75")]
+[assembly: AssemblyVersion("1.0.74.*")]
+[assembly: AssemblyFileVersion("1.0.74")]
